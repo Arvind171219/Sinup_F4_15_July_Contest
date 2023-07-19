5a8769b7d35af023c0e2eb7ac0c021506a778957
Arvind Kumar <arvindkr40882@gmail.com>
2023-07-19 12:17:11 +0530

    first commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+export default function Navbar() {
+  return (
+    <div className='navbar'>
+      <h1>Arvind Arya</h1>
+      <div className='menu'>
+      <a href="#"> Home </a>
+      <a href="#"> About Us</a>
+      <a href="#"> Contact </a>
+      <a href="#"> Skills </a>
+      <a href="#"> Qualification </a>
+      </div>
+    </div>
+  )
+}
